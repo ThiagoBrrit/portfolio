e4bba80146050583f204a8794db00f770bd85c98
Thiago Brito <deoliveira.tbrito@gmail.com>
2023-08-15 20:07:46 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/ThiagoBrrit.png" />
-    <Titulo fontSize={20}>Thiago Brito</Titulo>
+    <SidebarContainer>
+      <Avatar></Avatar>
+      <Titulo fontSize={20}>Thiago Brito</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        ThiagoBrrit
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Futuro Dev Full stack
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
